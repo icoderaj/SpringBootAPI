8ef81fe38ac328eb46aabde8dee999145bee86ae
icoderaj <rajprusty84@gmail.com>
2021-02-27 22:06:49 +0530

    icoderaj implemented REST API CRUD course application with SpringBoot with MySql DB

--- springrest/src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
+++ springrest/src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -3,32 +3,37 @@ package com.springrest.springrest.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springrest.springrest.dao.CourseDao;
 import com.springrest.springrest.entities.Course;
 
 @Service
 public class CourseServiceImpl implements CourseService{
 
-	List<Course> list;
+	//List<Course> list;
 	
 	
 	
-	public CourseServiceImpl() {
+	
+	
+	/*public CourseServiceImpl() {
 		list = new ArrayList<>();
 		list.add(new Course(100,"Java core course", "contains java basics"));
 		list.add(new Course(200,"Java advance course", "contains java advanced"));
 		list.add(new Course(300,"C  course", "contains C "));
 		list.add(new Course(400,"C++  course", "contains C++ "));
 		
-	}
+	}*/
 
-
+	@Autowired
+	private CourseDao courseDao;
 
 	@Override
 	public List<Course> getCourses() {
 		// TODO Auto-generated method stub
-		return list;
+		return courseDao.findAll();
 	}
 
 
@@ -37,15 +42,15 @@ public class CourseServiceImpl implements CourseService{
 	public Course getCourse(long courseId) {
 		// TODO Auto-generated method stub
 		
-		Course c = null;
+		/*Course c = null;
 		for(Course course:list)
 		{
 			if(course.getId()==courseId){
 				c = course;
 				break;
 			}
-		}
-		return c;
+		}*/
+		return courseDao.getOne(courseId);
 	}
 
 
@@ -54,7 +59,8 @@ public class CourseServiceImpl implements CourseService{
 	public Course addCourse(Course course) {
 		// TODO Auto-generated method stub
 		
-		list.add(course);
+		/*list.add(course);*/
+		courseDao.save(course);
 		return course;
 	}
 
@@ -64,7 +70,7 @@ public class CourseServiceImpl implements CourseService{
 	public Course deleteCourse(long courseId) {
 		// TODO Auto-generated method stub
 		
-		Course c = null;
+		/*Course c = null;
 		for(Course course:list)
 		{
 			if(course.getId()==courseId){
@@ -72,8 +78,11 @@ public class CourseServiceImpl implements CourseService{
 				list.remove(course);
 				break;
 			}
-		}
-		return c;
+		}*/
+		
+		Course entity = courseDao.getOne(courseId);
+		courseDao.delete(entity);
+		return entity;
 	}
 
 
@@ -81,13 +90,14 @@ public class CourseServiceImpl implements CourseService{
 	@Override
 	public Course updateCourse(Course course) {
 		// TODO Auto-generated method stub
-		list.forEach(e->
+		/*list.forEach(e->
 		{if(e.getId()==course.getId())
 			{
 				e.setTitle(course.getTitle());
 				e.setDescription(course.getDescription());
 			}
-			});
+			});*/
+		courseDao.save(course);
 		
 		return course;
 	}
