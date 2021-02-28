c6c578dbcdb0cda0b78f8857c7a470de78fbf9ff
icoderaj <rajprusty84@gmail.com>
2021-02-28 14:07:33 +0530

    icoderaj implemented Spring Boot to Consume external REST APIs and give JSON data

---
+++ springbootconsumeapi/src/test/java/com/springbootapi/springbootconsumeapi/SpringbootconsumeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootapi.springbootconsumeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootconsumeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
